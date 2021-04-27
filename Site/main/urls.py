8c8d59c430b3412250d83d582a42863a8b676ce5
Bonga <dolbanovich@list.ru>
2021-04-27 15:38:43 +0500

    123

---
+++ Site/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about'),
+    path('web-cam', views.livefe, name='camera')
+]
