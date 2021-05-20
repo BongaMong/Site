3a8d9a79b18e4a573105e23bf727fd792ed51ca3
Bonga <dolbanovich@list.ru>
2021-05-21 00:05:47 +0500

    Revert "123"
    This reverts commit 8c8d59c430b3412250d83d582a42863a8b676ce5.

--- Site/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about-us', views.about, name='about'),
-    path('web-cam', views.livefe, name='camera')
-]
